724dc36afd9ede56fb2b9d9a627c84282a3482f6
Adriel Estoller <adrielestoller@nave.org.br>
2021-09-29 23:28:50 -0300

    Estrutura de pastas criada + Configuração do React Navigation e criação das rotas.

---
+++ src/pages/editTask/editTask.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function editTask() {
+    return (
+        <View>
+            <Text>Edite Task</Text>
+        </View>
+    )
+}
+
+export default editTask
---
+++ src/routes/routes.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import homeScreen from './../pages/home/home';
+import addTaskScreen from './../pages/addTask/addTask';
+import editTaskScreen from './../pages/editTask/editTask';
+
+const Stack = createNativeStackNavigator();
+
+function routes() {
+    return (
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Home'>
+                <Stack.Screen 
+                    name='Home'
+                    component={homeScreen}
+                    options={{
+                        title: 'Tarefas',
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: {
+                          fontWeight: 'bold',
+                        },
+                      }}
+                />
+                <Stack.Screen 
+                    name='AddTask'
+                    component={addTaskScreen}
+                    options={{
+                        title: 'Adicione uma tarefa',
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: {
+                          fontWeight: 'bold',
+                        },
+                      }}
+                />
+                <Stack.Screen 
+                    name='EditTask'
+                    component={editTaskScreen}
+                    options={{
+                        title: 'Edite esta tarefa',
+                        headerTitleAlign: 'center',
+                        headerTitleStyle: {
+                          fontWeight: 'bold',
+                        },
+                      }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default routes;
